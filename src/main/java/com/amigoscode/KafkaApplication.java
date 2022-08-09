0c59e8d37c5a7ae1f2ff08f21e299701a786a50e
Guilherme Costa <glmribeiroc@gmail.com>
2022-08-09 17:58:09 -0300

    primeiro commit

---
+++ src/main/java/com/amigoscode/KafkaApplication.java
@@ -0,0 +1,25 @@
+package com.amigoscode;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, Message> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("amigoscode", new Message("Hello kafka !!", LocalDateTime.now()));
+		};
+	}
+
+}
